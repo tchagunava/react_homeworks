8ec1356d2ec994d4a10a18a59966cebd89afc3a7
loocdoc <tokochagunava@yahoo.com>
2023-01-11 13:29:30 +0400

    react_app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import "./index.css";
+import Parent from './Parent'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parent />
     </div>
   );
 }
 
 export default App;
+// პირველი დავალება
+// function App() {
+//   const Users = [
+//     { name: 'john', lname: 'smith', age: 18, id: 1 },
+//     { name: 'tom', lname: 'hertz', age: 19, id: 2 },
+//     { name: 'ben', lname: 'bright', age: 20, id: 3 },
+//     { name: 'don', lname: 'leam', age: 21, id: 4 },
+//     { name: null, lname: null, age: null, id: null },
+
+//   ];
+
+//   return (
+// //     <div>
+//       {Users.map((user) => {
+//         return <h1>{user.name}, {user.lname}, {user.age}, {user.id}</h1>
+//       })}
+// //     </div>
+//   )
+
+//   function Child(Users, isUserLoggedin) {
+//     if (isUserLoggedin = true) {
+//       return (
+//         <div>
+//         </div>
+//       )
+//     }
+//     <div>
+//       no users in the systems
+//     </div>
+//   }
+
+// }
+
+
+// მეორე დავალება
+// function Wrapper() {
+//   return (
+//     <div>
+//       <p>just text</p>
+//     </div>
+//   )
+// }
+
+// function Navbar() {
+//   return (
+//     <div>
+//       <p>just text</p>
+//     </div>
+//   )
+// }
+
+// function Footer() {
+//   return (
+//     <div>
+//       <p>just text</p>
+//     </div>
+//   )
+
+// }
+
+
---
+++ src/Child.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Child = (props) => {
+    return (
+        <h2> {props.data} </h2>
+    );
+}
+
+export default Child;
